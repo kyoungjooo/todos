39bb5134cdc291c3c17655e98c4086c406ee25bd
leekjoo <leekjoo1008@gmail.com>
2024-12-11 21:50:31 +0900

    feat : edit

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,25 +1,49 @@
-const TodoItem = ({ todos }) => {
+import { useState } from "react";
+
+const TodoItem = ({ todo, getUpdatedValue, getDeleteTarget }) => {
+  const { id } = todo;
+  const [isEditing, setEditing] = useState(true); //읽기모드
+  const [updated, setUpdated] = useState(todo);
+
+  const handleEdit = () => setEditing((prev) => !prev); //읽기모드취소
+
+  const handleValue = (e) => {
+    const { name, value } = e.target;
+    setUpdated({ ...updated, [name]: value });
+    if (!isEditing) getUpdatedValue(updated);
+    //편집이 끝나면 updateInputValue에 값을 전달한다.
+  };
+
+  const handleDelete = (todo) => getDeleteTarget(todo.id);
+
   return (
-    <ul>
-      {todos.map((todo) => {
-        const { id, title, content } = todo;
-        return (
-          <li key={id}>
-            <div>
-              <input type="checkbox" name="checkBox" id={id} />
-              <div>
-                <button type="button">수정</button>
-                <button type="button">삭제</button>
-              </div>
-            </div>
-            <label htmlFor={id}>
-              <span>{title}</span>
-              <p>{content}</p>
-            </label>
-          </li>
-        );
-      })}
-    </ul>
+    <li key={id}>
+      <input type="checkbox" name="checkBox" id={id} />
+      <div>
+        <button type="button" onClick={() => handleEdit(todo)}>
+          수정
+        </button>
+        <button type="button" onClick={() => handleDelete(todo)}>
+          삭제
+        </button>
+      </div>
+
+      <label htmlFor={id}>
+        <input
+          type="text"
+          name="title"
+          value={updated.title}
+          disabled={isEditing}
+          onChange={handleValue}
+        />
+        <textarea
+          name="content"
+          value={updated.content}
+          disabled={isEditing}
+          onChange={handleValue}
+        />
+      </label>
+    </li>
   );
 };
 export default TodoItem;
--- src/pages/todoMain.jsx
+++ src/pages/todoMain.jsx
@@ -10,11 +10,34 @@ const TodoMain = () => {
     const { id, title, content } = newTodo;
     setTodos((prev) => [...prev, { id, title, content }]);
   };
-  console.log(todos);
+
+  //수정된 투두로 변경하기위해 가져오기
+  const getUpdatedValue = (updated) => {
+    setTodos((prev) =>
+      prev.map((todo) =>
+        todo.id === updated.id ? { ...todo, ...updated } : todo
+      )
+    );
+  };
+
+  const getDeleteTarget = (deleted) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== deleted));
+  };
 
   return (
     <>
-      <TodoItem todos={todos} />
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              getUpdatedValue={getUpdatedValue}
+              getDeleteTarget={getDeleteTarget}
+            />
+          );
+        })}
+      </ul>
       <Input getTextFromInput={getTextFromInput} />
     </>
   );
